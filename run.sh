26960dc20cf6d476662660ce7db985b4c39c7151
F45 <38011761+MrF45@users.noreply.github.com>
2018-04-03 16:12:43 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,13 +4,16 @@
 
 
 #List
+clear
+echo "List Tools"
 echo "01.Lazymux"
 echo "02.Vbug"
 echo "03.Speedtest-cli"
 echo "04.Allinwan"
 echo "05.Sn1per"
 echo "06.Toolss"
-echo "07.Exit"
+echo "07.Yuuki-Pentest"
+echo "08.Exit"
 read up
 
 if [ $up = 1 ] || [ $up = 01 ]
@@ -58,7 +61,8 @@ git clone https://github.com/verluchie/allinwan
 fi
 
 if [ $up = 5 ] || [ $up = 05 ]
-clear echo " Installing "
+clear 
+echo" Installing "
 sleep 1
 apt update && apt upgrade -y
 apt-get install git -y
@@ -66,6 +70,9 @@ git clone https://github.com/1N3/Sn1per
 fi
 
 if [ $up = 6 ] || [ $up = 06  ]
+clear
+echo " Installing "
+sleep 1
 apt update && apt upgrade -y
 apt-get install python -y
 apt-get install git -y
@@ -73,6 +80,15 @@ git clone https://github.com/AnonHackerr/toolss
 fi
 
 if [ $up = 7 ] || [ $up = 07  ]
+clear
+echo " Installing "
+sleep 1
+apt update && apt upgrade -y
+apt-get install git
+git clone https://github.com/Yukinoshita47/Pentest-Tools-Auto-Installer.git
+fi
+
+if [ $up = 8 ] || [ $up = 08  ]
 then
 echo " Exit "
 sleep 1
